f4433507affb442f4f25449f16156c346b0b1054
master <qd_usb@163.com>
2015-02-01 15:32:45 +0800

    air

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -7,11 +7,12 @@ update 2014.6.10
 class IndexAction extends BasicAction {
     public function index(){
        
-        $banners=M("link")->where("class_id=1")->select();
-        $this->assign("banner",$banners);
+        $banners=M("banner")->where("class_id=1")->select();
+        $this->assign("banners",$banners);
 
         $links=M("link")->where("class_id=1")->select();
         $this->assign("links",$links);
+
 		$this->assign("title","网站首页-".C("CONFIG_TITLE"));
 		$this->display("default");
     }
